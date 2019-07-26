21f1a6aa5fcaa63b3a720a3981d8c78fc6f21b5f
zloyleva <zloyleva.imt@gmail.com>
2019-07-26 19:37:01 +0300

    add attributes for Html creator

--- scripts/app.js
+++ scripts/app.js
@@ -12,46 +12,67 @@ const users = [
     {name: 'Serj', gender: 'male', salary: '2000'},
 ];
 
-const createElement = (tag, content) => {
-    const element = document.createElement(tag);
-    element.innerText = content;
 
-    return element;
-}
 
-const appendArray = (htmlEl, arrayEls) => {
-    arrayEls.map(el => htmlEl.appendChild(el))
-    return htmlEl;
-}
+const render = data => {
+    const createElement = (tag, content, className=null, attr=[]) => {
+        const element = document.createElement(tag);
+        
+        if(typeof content == 'object'){
+            element.appendChild(content)
+        }else{
+            element.innerText = content;
+        }
 
-const innerTablesRows = users.map((el, i) => {
-    const index = createElement('th',i + 1);
-    const name = createElement('td',el.name);
-    const gender = createElement('td',el.gender);
-    const salary = createElement('td',el.salary);
+        if(attr.length){
+            attr.map(el => element.setAttribute(el.name,el.value))
+        }
+        
+        element.className = className?className:'';
     
-    return appendArray(
-        document.createElement('tr'),
-        [index,name,gender,salary]
+        return element;
+    }
+    
+    const appendArray = (htmlEl, arrayEls) => {
+        arrayEls.map(el => htmlEl.appendChild(el))
+        return htmlEl;
+    }
+    
+    const innerTablesRows = data.map((el, i) => {
+        return appendArray(
+            document.createElement('tr'),
+            [
+                {tag:'th',value:i + 1},
+                {tag:'td',value:el.name},
+                {tag:'td',value:el.gender},
+                {tag:'td',value:el.salary},
+                {tag:'td',value:createElement('button','Delete','btn btn-danger',[{name:'id',value:i}])},
+            ].map(el => createElement(el.tag,el.value))
+        );
+    });
+    
+    
+    const tbody = document.createElement('tbody')
+    innerTablesRows.map(el => tbody.appendChild(el));
+    
+    
+    const tr = appendArray(document.createElement('tr'),[
+            '#','Name','Gender','Salary','Action',
+        ].map(el => createElement('th',el))
     );
-});
+    
+    const thead = appendArray(document.createElement('thead'),[tr]);
+    
+    const table = appendArray(createElement('table','','table'),[
+        thead,tbody
+    ]);
+    
+    document.getElementsByClassName('col-12')[0].appendChild(table);
+}
 
+render(users);
 
-const tbody = document.createElement('tbody')
-innerTablesRows.map(el => tbody.appendChild(el));
-
-
-const tr = appendArray(document.createElement('tr'),[
-    createElement('th','#'),
-    createElement('th','Name'),
-    createElement('th','Gender'),
-    createElement('th','Salary'),
-]);
-
-const thead = appendArray(document.createElement('thead'),[tr]);
-
-const table = appendArray(document.createElement('table'),[
-    thead,tbody
-]);
-
-document.getElementsByClassName('col-12')[0].appendChild(table);
+[...document.querySelectorAll('button')]
+    .map(el => el.addEventListener('click', (e) => {
+        console.log('CLICK', e.target.getAttribute('id'))
+    }))
