537368626c435b6f9d9f4087fc3a33e1db7873d4
zloyleva <zloyleva.imt@gmail.com>
2019-07-26 20:43:51 +0300

    loader

--- scripts/app.js
+++ scripts/app.js
@@ -1,37 +1,52 @@
-const users = [
-    {name: 'Mark', gender: 'male', salary: '1500'},
-    {name: 'Igor', gender: 'male', salary: '500'},
-    {name: 'Anna', gender: 'female', salary: '1000'},
-    {name: 'John', gender: 'male', salary: '2100'},
-    {name: 'Oleh', gender: 'male', salary: '3000'},
-    {name: 'Mary', gender: 'female', salary: '1500'},
-    {name: 'Pit', gender: 'male', salary: '750'},
-    {name: 'Bred', gender: 'male', salary: '1050'},
-    {name: 'Tom', gender: 'male', salary: '1500'},
-    {name: 'Jery', gender: 'female', salary: '1800'},
-    {name: 'Serj', gender: 'male', salary: '2000'},
-];
+let users = [];
+const createElement = (tag, content, className=null, attr=[]) => {
+    const element = document.createElement(tag);
+    
+    if(typeof content == 'object'){
+        element.appendChild(content)
+    }else{
+        element.innerText = content;
+    }
 
+    if(attr.length){
+        attr.map(el => element.setAttribute(el.name,el.value))
+    }
+    
+    element.className = className?className:'';
 
+    return element;
+}
+{/* <i class="fas fa-spinner fa-spin"></i> */}
+
+const getUsersData = () => {
+    document.getElementsByClassName('col-12')[0].innerHTML = '';
+    document.getElementsByClassName('col-12')[0].appendChild(
+        createElement('i','','fas fa-spinner fa-spin')
+    )
+    .appendChild(
+        createElement('span','Loading...')
+    )
+    setTimeout(()=>{
+        users = [
+            {name: 'Mark', gender: 'male', salary: '1500'},
+            {name: 'Igor', gender: 'male', salary: '500'},
+            {name: 'Anna', gender: 'female', salary: '1000'},
+            {name: 'John', gender: 'male', salary: '2100'},
+            {name: 'Oleh', gender: 'male', salary: '3000'},
+            {name: 'Mary', gender: 'female', salary: '1500'},
+            {name: 'Pit', gender: 'male', salary: '750'},
+            {name: 'Bred', gender: 'male', salary: '1050'},
+            {name: 'Tom', gender: 'male', salary: '1500'},
+            {name: 'Jery', gender: 'female', salary: '1800'},
+            {name: 'Serj', gender: 'male', salary: '2000'},
+        ];
+        console.log('uploaded')
+        render(users);
+    },2000)
+}
 
 const render = data => {
-    const createElement = (tag, content, className=null, attr=[]) => {
-        const element = document.createElement(tag);
-        
-        if(typeof content == 'object'){
-            element.appendChild(content)
-        }else{
-            element.innerText = content;
-        }
-
-        if(attr.length){
-            attr.map(el => element.setAttribute(el.name,el.value))
-        }
-        
-        element.className = className?className:'';
     
-        return element;
-    }
     
     const appendArray = (htmlEl, arrayEls) => {
         arrayEls.map(el => htmlEl.appendChild(el))
@@ -39,6 +54,11 @@ const render = data => {
     }
     
     const innerTablesRows = data.map((el, i) => {
+        const button = createElement('button','Delete','btn btn-danger',[{name:'id',value:i}]);
+        
+        button.addEventListener('click', (e) => {
+            render(data.filter((el,j) => i != j));
+        })
         return appendArray(
             document.createElement('tr'),
             [
@@ -46,7 +66,7 @@ const render = data => {
                 {tag:'td',value:el.name},
                 {tag:'td',value:el.gender},
                 {tag:'td',value:el.salary},
-                {tag:'td',value:createElement('button','Delete','btn btn-danger',[{name:'id',value:i}])},
+                {tag:'td',value:button},
             ].map(el => createElement(el.tag,el.value))
         );
     });
@@ -66,13 +86,20 @@ const render = data => {
     const table = appendArray(createElement('table','','table'),[
         thead,tbody
     ]);
-    
+    document.getElementsByClassName('col-12')[0].innerHTML = '';
     document.getElementsByClassName('col-12')[0].appendChild(table);
 }
 
-render(users);
 
-[...document.querySelectorAll('button')]
-    .map(el => el.addEventListener('click', (e) => {
-        console.log('CLICK', e.target.getAttribute('id'))
-    }))
+
+getUsersData();
+
+// [...document.querySelectorAll('button')]
+//     .map(el => el.addEventListener('click', (e) => {
+//         console.log('CLICK', e.target.getAttribute('id'))
+
+//         console.log();
+
+        
+//         render(users.filter((el,i) => i != e.target.getAttribute('id')));
+//     }))
